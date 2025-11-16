cbc31b1e2bf8a9731db8b7d8349cecd1e024dec6
georgeslight <georgeslight@gmail.com>
2025-11-16 18:03:56 +0100

    helix language servers

--- app/helix/helix.nix
+++ app/helix/helix.nix
@@ -23,10 +23,32 @@
     pkgs.delve # Debugger
     pkgs.gotools # Go development tools (Formatter)
     pkgs.golangci-lint # Linter (for CLI)
+    # Go Templ
+    pkgs.templ
 
     # Nix
     pkgs.nil
     pkgs.alejandra
     pkgs.nixfmt-rfc-style
+
+    # Bash
+    pkgs.bash-language-server
+    # CSS ESLint HTML JSON MD
+    pkgs.vscode-langservers-extracted
+    # JS TS
+    pkgs.typescript-language-server
+    # TailwindCSS
+    pkgs.tailwindcss-language-server
+    # MD
+    pkgs.marksman
+    # Hypr
+    pkgs.hyprls
+    # TOML
+    pkgs.taplo
+    # YAML
+    pkgs.yaml-language-server
+    # Docker
+    pkgs.docker-compose-language-service
+    pkgs.dockerfile-language-server-nodejs
   ];
 }
