ffad8dac84578239cef097a787e5615201df939d
georgeslight <georgeslight@gmail.com>
2025-11-16 01:29:46 +0100

    cleanup

--- home.nix
+++ home.nix
@@ -40,59 +40,6 @@
     # '')
   ];
 
-  # programs.helix = {
-  #   enable = true;
-  #   package = inputs.helix.packages.${pkgs.system}.default;
-  #   # package = pkgs-unstable.helix; 
-
-  #   settings = {
-  #     theme = "tokyonight";
-
-  #     keys.normal = {
-  #       # Lazygit
-  #       "C-g" = [":new" ":insert-output lazygit" ":buffer-close!" ":redraw" ":rla"];
-  #       # Yazi
-  #       "C-y" = [
-  #       ":sh rm -f /tmp/unique-file"
-  #       ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
-  #       ":insert-output echo \"\\x1b[?1049h\\x1b[?2004h\" > /dev/tty"
-  #       ":open %sh{cat /tmp/unique-file}"
-  #       ":redraw"
-  #       ":set mouse false"
-  #       ":set mouse true"
-  #     ];
-        
-  #     };
-      
-  #     editor = {
-  #       lsp.display-inlay-hints = true;
-  #       line-number = "relative";
-  #       cursorline = true;
-  #       mouse = false;
-  #       color-modes = true;
-  #     };
-      
-  #     editor.cursor-shape = {
-  #       insert = "bar";
-  #       normal = "block";
-  #       select = "underline";
-  #     };
-      
-  #     editor.inline-diagnostics = {
-  #       cursor-line = "hint";
-  #     };
-      
-  #     keys.insert = {
-  #       j.k = "normal_mode";
-  #     };
-      
-  #     keys.normal.space = {
-  #       o = "file_picker_in_current_buffer_directory";
-  #     };
-      
-  #   };
-  # };
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
